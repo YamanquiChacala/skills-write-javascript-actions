ecaa20b93fdf58038c6e1f4dcb20fd00d59f21ef
Yamanqui <yamanqui@chacala.school>
2025-03-13 15:00:53 -0700

    creating joke.js and main.js

---
+++ .github/actions/joke-action/joke.js
@@ -0,0 +1,18 @@
+const request = requier("request-promise");
+
+const options = {
+    method: "GET",
+    uri: "ttps://icanhazdadjoke.com/",
+    headers: {
+        Accept: "application/json",
+        "User-Agent": "Writing Javascript action GitHub Skills course.",
+    },
+    json: true,
+};
+
+async function getJoke() {
+    const res = await request(options);
+    return res.joke;
+}
+
+module.exports = getJoke;
